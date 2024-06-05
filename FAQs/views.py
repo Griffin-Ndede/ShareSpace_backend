52067a16feeaf5e3dabe9015715aa14086941b62
Griffin <omondigriffin4@gmail.com>
2024-06-05 12:52:00 +0300

    creates an FAQs app

---
+++ FAQs/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def FAQs (request):
+    return HttpResponse("This is where my FAQs will be displayed")
