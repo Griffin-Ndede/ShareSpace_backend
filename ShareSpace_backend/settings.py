4c9bf42dc94fa84cf2d7a9b2d16e422f9cd59078
Griffin <omondigriffin4@gmail.com>
2024-06-05 16:40:03 +0300

    sets up CORS

--- ShareSpace_backend/settings.py
+++ ShareSpace_backend/settings.py
@@ -38,11 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # local
-    'FAQs',
-
     # 3rd party apps
     'rest_framework',
+    'corsheaders',
+
+    # local
+    'FAQs',
 ]
 
 # configuring the django rest framework
@@ -58,6 +59,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', #CORS
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,3 +139,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = (
+'http://localhost:3000',
+'http://localhost:8000',
+)
