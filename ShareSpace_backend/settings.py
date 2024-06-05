02457b12a22b0128c0f9f1d13670ccc9aa5bb352
Griffin <omondigriffin4@gmail.com>
2024-06-05 14:47:54 +0300

    creates a serializer file

--- ShareSpace_backend/settings.py
+++ ShareSpace_backend/settings.py
@@ -37,8 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FAQs'
+
+    # local
+    'FAQs',
+
+    # 3rd party apps
+    'rest_framework',
 ]
+
+# configuring the django rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
