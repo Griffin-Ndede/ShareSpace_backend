a14a51138de3374f62543f5b2059ae3f34cb56c5
Griffin <omondigriffin4@gmail.com>
2024-06-17 06:22:30 +0300

    creates necessary files for the emails app

--- ShareSpace_backend/urls.py
+++ ShareSpace_backend/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("",include("Resources.urls")),
+    path("", include("emails.urls")),
     path('admin/', admin.site.urls),
 
 ]  
