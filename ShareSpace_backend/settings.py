ffacb16f3c5b577536fb1b0b68fcaca32c1c011f
Griffin <omondigriffin4@gmail.com>
2024-07-23 06:07:00 +0300

    updates the database url

--- ShareSpace_backend/settings.py
+++ ShareSpace_backend/settings.py
@@ -119,8 +119,15 @@ WSGI_APPLICATION = 'ShareSpace_backend.wsgi.application'
 
 # replacing the sqlite database with postgresql 
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
+
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
