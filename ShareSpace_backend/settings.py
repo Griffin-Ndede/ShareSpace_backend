2d34258f14804d0b6fafa23d89aae30761832e78
Griffin <omondigriffin4@gmail.com>
2025-01-30 23:17:41 +0300

    initiates login and registration authentication

--- ShareSpace_backend/settings.py
+++ ShareSpace_backend/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
     'corsheaders',
     'Resources',
 ]
