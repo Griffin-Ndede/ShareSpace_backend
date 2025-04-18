c59995c44a805d857693156e04093cb62a3e8e59
Griffin <omondigriffin4@gmail.com>
2025-04-18 11:29:30 +0300

    modifications to the user registration app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from  rest_framework import status
+
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user= serializer.save()
+            refresh= RefreshToken.for_user(user)
+            return Response({
+                'refresh':str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
