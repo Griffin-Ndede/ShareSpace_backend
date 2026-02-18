4a404105c005d5bc34de9c6fa97caa0093562121
Griffin <omondigriffin4@gmail.com>
2026-02-18 17:54:56 +0300

    Refactor: updates the listing model to accommodate new listing fields

--- Apps/Listings/models.py
+++ Apps/Listings/models.py
@@ -1,30 +1,39 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-import cloudinary
-from cloudinary.models import CloudinaryField
 
 User = get_user_model()
 
 
-# Create your models here.
 class Listing(models.Model):
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="listings", null=True
     )
+
+    # Step 1
     category = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image_url = models.JSONField(default=list, blank=True)
-    startDate = models.DateField(null=True)
-    endDate = models.DateField(null=True)
     condition = models.CharField(max_length=255)
+    accessories = models.CharField(max_length=255, blank=True, null=True)
+    usage_rules = models.TextField(blank=True, null=True)
+
+    # Step 2
+    image_url = models.JSONField(default=list, blank=True)
     location = models.CharField(max_length=255)
-    deposit = models.CharField(max_length=255, null=True)
-    accessories = models.CharField(max_length=255, null=True)
-    # name = models.CharField(max_length=255)
-    # phoneNumber = models.IntegerField(null=True)
-    # emailAddress = models.CharField(max_length=255, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    startDate = models.DateField(null=True, blank=True)
+    endDate = models.DateField(null=True, blank=True)
+
+    # Step 3
+    dailyRate = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    weeklyRate = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
+    minPeriod = models.CharField(max_length=50, null=True, blank=True)
+    deposit = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    depositRequirements = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.title
