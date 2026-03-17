f50d333492f5d685c8807ea11d9cf30e8bf9b203
Griffin <omondigriffin4@gmail.com>
2026-03-17 15:29:57 +0300

    Fix: configures render for handling the database

--- ShareSpace_backend/settings.py
+++ ShareSpace_backend/settings.py
@@ -104,11 +104,9 @@ REST_FRAMEWORK = {
 #     }
 # }
 
+
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL", ""),
-        conn_max_age=600,
-    )
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Cloudinary configuration
